ad2c56205eff2ae656d798dc1ea7b633cfc958ba
Zorca Orcinus <vs@zorca.org>
2024-03-23 14:23:39 +0200

    add: file upload controller, form and form request for validation

--- app/Http/Controllers/ManageExcelFiles.php
+++ app/Http/Controllers/ManageExcelFiles.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UploadExcelFileRequest;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -26,9 +27,9 @@ class ManageExcelFiles extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(UploadExcelFileRequest $uploadExcelFileRequest)
     {
-        //
+        return back();
     }
 
     /**
--- resources/js/Pages/ManageExcelFiles/Index.vue
+++ resources/js/Pages/ManageExcelFiles/Index.vue
@@ -1,7 +1,15 @@
 <script setup>
+import { reactive } from "vue";
+import { router, useForm } from '@inertiajs/vue3';
 import DefaultLayout from '@/Layouts/DefaultLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
-import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+
+const form = useForm({
+    file: null,
+});
+
+function store() {
+    form.post(route('manage-excel-files.store'));
+}
 </script>
 
 <template>
@@ -17,16 +25,14 @@ import ApplicationLogo from "@/Components/ApplicationLogo.vue";
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
                     <div>
                         <div class="p-6 lg:p-8 bg-white dark:bg-gray-800 dark:bg-gradient-to-bl dark:from-gray-700/50 dark:via-transparent border-b border-gray-200 dark:border-gray-700">
-
-                            <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-                                Welcome to your Jetstream application!
-                            </h1>
-
-                            <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
-                                Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-                                to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-                                you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-                                ecosystem to be a breath of fresh air. We hope you love it.
+                            <p class="max-w-lg mx-auto mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
+                                <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">Upload XLS or XLSX files</h1>
+                                <form @submit.prevent="store">
+                                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file">Upload file</label>
+                                    <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_help" id="file" type="file" @input="form.file = $event.target.files[0]">
+                                    <p v-if="form.errors.file" class="mt-2 text-sm text-red-600 dark:text-red-500">{{ form.errors.file }}</p>
+                                    <button type="submit" class="mt-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Upload</button>
+                                </form>
                             </p>
                         </div>
                     </div>

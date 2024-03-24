fa57a0a52d54fc64e0cf788b05c0a3848066e266
Zorca Orcinus <vs@zorca.org>
2024-03-24 15:16:06 +0200

    fix: route in layout

--- resources/js/Layouts/DefaultLayout.vue
+++ resources/js/Layouts/DefaultLayout.vue
@@ -23,7 +23,7 @@ defineProps({
                         <div class="flex">
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
-                                <Link :href="route('dashboard')">
+                                <Link href="/">
                                     <ApplicationMark class="block h-9 w-auto" />
                                 </Link>
                             </div>

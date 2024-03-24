6c1c9f754393cfe91702e3de38f8b99a19229f7b
Zorca Orcinus <vs@zorca.org>
2024-03-24 09:47:38 +0200

    install: flowbite vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,8 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
+import '../../node_modules/flowbite-vue/dist/index.css'
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
